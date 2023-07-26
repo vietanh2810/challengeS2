361fcc0658416ad9c1d3a0f922648c65b7cd6f4b
vietanh2810 <vietanhc12@gmail.com>
2023-07-26 17:06:01 +0200

    admin page + validation

--- frontend/frontend/src/services/user.service.js
+++ frontend/frontend/src/services/user.service.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 import authHeader from './auth-header';
 
-const API_URL = import.meta.env.VUE_APP_API_ENDPOINT;
+const API_URL = 'http://localhost:8080/api';
 
 class UserService {
     validateUser(userId) {
-        return axios.put(API_URL + '/users/validate' + userId, { headers: authHeader() });
+        return axios.put(API_URL + '/users/validate/' + userId, { headers: authHeader() });
     }
 
     getAdminBoard() {
