eaff0ae8ffc585d0bc1bc61dd604f901c79af756
Honzikoi <honza.doan@gmail.com>
2023-07-28 01:43:01 +0200

    hotfix #3

--- frontend/frontend/vite.config.js
+++ frontend/frontend/vite.config.js
@@ -1,7 +1,7 @@
 import { fileURLToPath, URL } from "url";
-
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+const path = require('node:path'); 
 
 // https://vitejs.dev/config/
 export default defineConfig({
