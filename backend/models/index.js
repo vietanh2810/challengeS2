c18c8a93cfc67844dca0cedc3fe1b66c435761b7
Yann Doan <honza.doan@gmail.com>
2023-07-27 18:09:14 +0200

    testing connections to DBs for deployment

--- backend/models/index.js
+++ backend/models/index.js
@@ -5,11 +5,11 @@ const { Sequelize, DataTypes } = require("sequelize");
 //port for my database is 5433
 //database name is discover
 const sequelize = new Sequelize({
-  host: "localhost",
+  host: "dpg-cj199pa7l0ft7nl7lot0-a",
   port: 5432,
-  database: "postgres",
-  username: "postgres",
-  password: "123456",
+  database: "acpostgresdb",
+  username: "honzikoi",
+  password: "fP4nPtvwM6dzuNMDRhRE0niKhaU5pUq",
   dialect: "postgres",
   pool: {
     max: 10,
@@ -33,17 +33,15 @@ const db = {};
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
 
-
-db.users = require('./userModel')(sequelize, DataTypes)
-db.companies = require('./companyModel')(sequelize, DataTypes)
-db.websites = require('./websiteModel')(sequelize, DataTypes)
-db.tags = require('./tagModel')(sequelize, DataTypes)
+db.users = require("./userModel")(sequelize, DataTypes);
+db.companies = require("./companyModel")(sequelize, DataTypes);
+db.websites = require("./websiteModel")(sequelize, DataTypes);
+db.tags = require("./tagModel")(sequelize, DataTypes);
 db.kpis = require("./kpiModel")(sequelize, DataTypes);
 db.graphes = require("./grapheModel")(sequelize, DataTypes);
 db.heatmaps = require("./heatmapModel")(sequelize, DataTypes);
-db.conversionFunnel = require('./conversionFunnelModel')(sequelize, DataTypes)
+db.conversionFunnel = require("./conversionFunnelModel")(sequelize, DataTypes);
 // db.conversionFunnelTag = require('./conversionFunnelTagModel')(sequelize, DataTypes)
-
 
 //exporting the module
 module.exports = db;
