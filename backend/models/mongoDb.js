7b2b0cb3fdc4fc21615bf69d22dcdbe5688b02c1
Yann Doan <honza.doan@gmail.com>
2023-07-27 18:04:06 +0200

    revert db connection for develop branch

--- backend/models/mongoDb.js
+++ backend/models/mongoDb.js
@@ -28,24 +28,24 @@ const customEventSchema = new mongoose.Schema({
 const SessionEvent = mongoose.model("SessionEvent", sessionEventSchema);
 const CustomEvent = mongoose.model("CustomEvent", customEventSchema);
 
-// // Establish the MongoDB connection
-// mongoose.connect("mongodb://localhost:27017/mydatabase", {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// });
+// Establish the MongoDB connection
+mongoose.connect("mongodb://localhost:27017/mydatabase", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-const connectDB = async () => {
-  try {
-    await mongoose.connect(
-      "mongodb+srv://honzikoi:SuperTestPass123@analytic-challenge.2q15bqi.mongodb.net/?retryWrites=true&w=majority"
-    );
-    console.log("Connected to MongoDB");
-  } catch (e) {
-    console.log("Error connecting to MongoDB", e);
-  }
-};
+// const connectDB = async () => {
+//   try {
+//     await mongoose.connect(
+//       "mongodb+srv://honzikoi:SuperTestPass123@analytic-challenge.2q15bqi.mongodb.net/?retryWrites=true&w=majority"
+//     );
+//     console.log("Connected to MongoDB");
+//   } catch (e) {
+//     console.log("Error connecting to MongoDB", e);
+//   }
+// };
 
-connectDB();
+// connectDB();
 
 module.exports = {
   SessionEvent,
